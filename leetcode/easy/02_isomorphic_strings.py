d82533e407286f25f92fcc54d2da6c47504aac3c
wegar2 <awegrzyn17@gmail.com>
2026-07-05 23:27:28 +0200

    isomorphic strings sol added

---
+++ leetcode/easy/02_isomorphic_strings.py
@@ -0,0 +1,17 @@
+
+def is_isomorphic(s: str, t: str) -> bool:
+    m: dict[str, str] = {}
+    for i in range(len(s)):
+        if s[i] not in m:
+            if t[i] not in m.values():
+                m[s[i]] = t[i]
+            else:
+                return False
+        else:
+            if t[i] != m[s[i]]:
+                return False
+    return True
+
+
+if __name__ == "__main__":
+    print(is_isomorphic(s="egg", t="add"))
