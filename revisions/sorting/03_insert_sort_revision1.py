d19c31ec72929f887ba4bfca9dc645f0d1696ff0
wegar2 <awegrzyn17@gmail.com>
2026-07-07 18:57:14 +0200

    revision of insert sort

--- revisions/sorting/03_insert_sort_revision1.py
+++ revisions/sorting/03_insert_sort_revision1.py
@@ -1,4 +1,21 @@
 
 
 def insert_sort(values: list[int]) -> list[int]:
-    pass
+    for i in range(1, len(values), 1):
+
+        val = values[i]
+        j = i - 1
+
+        while j >= 0 and values[j] > val:
+            values[j + 1] = values[j]
+            j -= 1
+        values[j + 1] = val
+
+    return values
+
+
+if __name__ == "__main__":
+
+    values = [19, 4, 1, 10, 6, 23, 33, 7, 23, 34, 909, 65, 101]
+    res = insert_sort(values)
+    print(f"{res=}")
