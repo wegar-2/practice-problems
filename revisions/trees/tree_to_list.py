a5419773f19c17e150b167fa368574c6a8b07383
wegar2 <awegrzyn17@gmail.com>
2026-07-15 23:32:43 +0200

    added tree_to_list

--- revisions/trees/tree_to_list.py
+++ revisions/trees/tree_to_list.py
@@ -1,11 +1,29 @@
 from revisions.trees.node import Node
-from typing import Optional
 from collections import deque
 from revisions.trees.tree_util import make_tree
 
 
-def tree_to_list(root: Node | None) -> list[int | None]:
-    pass
+def tree_to_list(root: Node) -> list[int | None]:
+
+    queue: deque[Node | None] = deque()
+    queue.append(root)
+    out: list[int | None] = []
+
+    while queue:
+        node = queue.popleft()
+
+        if node is None:
+            out.append(None)
+            continue
+
+        out.append(node.value)
+        queue.append(node.left)
+        queue.append(node.right)
+
+    while out[-1] is None:
+        out.pop(-1)
+
+    return out
 
 
 if __name__ == "__main__":
