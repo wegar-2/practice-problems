3e4f03d27480554ff745fd5a5036ceae83b41330
wegar2 <awegrzyn17@gmail.com>
2026-07-22 21:55:43 +0200

    added sample solution to permutations problem

--- techniques/backtracking/permutations/01_backtracking_permutations.py
+++ techniques/backtracking/permutations/01_backtracking_permutations.py
@@ -1,23 +1,31 @@
 
 
-def _make_permutations(
-        nums: list[int],
-        start: int,
-        perm: list[int],
-        all_perms: list[int]
-) -> list[list[int]]:
-    pass
+def make_permutations(n: int = 4) -> list[list[int]]:
 
-
-
-
-def make_permutations(n: int = 5):
-
+    out: list[list[int]] = []
     nums: list[int] = list(range(1, n + 1, 1))
 
-    return _make_permutations(
-        nums=nums,
-        start=0,
-        perm=[],
-        all_perms=[]
-    )
+    def _make_permutations(current: list[int], used: list[bool]):
+        if len(current) == n:
+            out.append([x for x in current])
+        else:
+            for i in range(n):
+                if not used[i]:
+                    current.append(nums[i])
+                    used[i] = True
+                    _make_permutations(current, used)
+
+                    current.pop()
+                    used[i] = False
+
+    _make_permutations([], [False for _ in range(n)])
+
+    return out
+
+
+if __name__ == "__main__":
+
+    res = make_permutations(n=4)
+    print(f"{len(res)=}")
+    print(f"{res=}")
+
