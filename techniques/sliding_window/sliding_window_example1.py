69661ac7905d604821179a112a033d85c938d53b
wegar2 <awegrzyn17@gmail.com>
2026-08-12 23:01:42 +0200

    further optimized the example of use of sliding window for scanning a string

--- techniques/sliding_window/sliding_window_example1.py
+++ techniques/sliding_window/sliding_window_example1.py
@@ -1,6 +1,3 @@
-import random
-
-from utils.utils import make_random_string
 
 
 def shortest_containing_substring(
@@ -9,20 +6,16 @@ def shortest_containing_substring(
         k: int
 ) -> str:
 
-    # Complexity wise - I am using the notation:
-    # len(s) = n
-
-    if len(letter) > 1:
+    if len(letter) != 1:
         raise ValueError(f"Invalid {letter=} - string of len gt 1! ")
 
     if len(s) == 0:
         return ""
 
-    # this is O(n) - not very nice, although this does not change the complexity!
-    if letter not in s:
-        return ""
+    if k <= 0:
+        raise ValueError(f"{k=} has to be positive!")
 
-    scs: str = ""
+    scs_i, scs_j = 0, 0
     count_: int = 0
 
     i = 0
@@ -31,18 +24,14 @@ def shortest_containing_substring(
         if x == letter:
             count_ += 1
 
-        if count_ == k:
-            if scs == "":
-                scs = s[i:j+1]
-            else:
-                if j - i + 1 < len(scs):
-                    scs = s[i:j + 1]
-
         while count_ == k:
+            if scs_j - scs_i == 0:
+                scs_i, scs_j = i, j + 1
+            elif j - i + 1 < scs_j - scs_i:
+                scs_i, scs_j = i, j + 1
+
             if s[i] == letter:
                 count_ -= 1
             i += 1
-            if count_ == k and j - i + 1 < len(scs):
-                scs = s[i:j + 1]
 
-    return scs
+    return s[scs_i:scs_j]
