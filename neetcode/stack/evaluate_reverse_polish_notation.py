a3d7fa623c13ca1d9eb68802a1c1110edbaad603
wegar2 <awegrzyn17@gmail.com>
2026-08-13 20:45:43 +0200

    neetcode, stack, eval RPN

---
+++ neetcode/stack/evaluate_reverse_polish_notation.py
@@ -0,0 +1,30 @@
+from typing import List
+import math
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        stck: list[str] = []
+
+        for token in tokens:
+            if token in ["+", "-", "*", "/"]:
+                operand2: int = int(stck.pop())
+                operand1: int = int(stck.pop())
+                if token == "+":
+                    stck.append(operand1 + operand2)
+                elif token == "-":
+                    stck.append(operand1 - operand2)
+                elif token == "*":
+                    stck.append(operand1 * operand2)
+                else:
+                    res = operand1 / operand2
+                    if res > 0:
+                        res = math.floor(res)
+                    else:
+                        res = math.ceil(res)
+                    stck.append(res)
+            else:
+                stck.append(token)
+
+        return int(stck[0])
