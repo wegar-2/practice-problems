40b0a79260b49ba64a84d0625aa331b2ef7b691d
wegar2 <awegrzyn17@gmail.com>
2026-08-14 20:26:49 +0200

    neetcode, sliding window, sliding window maximum

--- neetcode/sliding_window/sliding_window_maximum.py
+++ neetcode/sliding_window/sliding_window_maximum.py
@@ -1,7 +1,19 @@
+import heapq
 from typing import List
 
 
 class Solution:
 
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+
+        out: list[int] = []
+        heap: list[tuple[int, int]] = []
+
+        for i, x in enumerate(nums):
+            heapq.heappush(heap, (-x, i))
+            if i >= k - 1:
+                while heap[0][1] > i or heap[0][1] < (i - k + 1):
+                    heapq.heappop(heap)
+                out.append(-heap[0][0])
+
+        return out
