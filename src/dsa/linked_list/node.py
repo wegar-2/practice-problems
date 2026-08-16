b8b8e80b5c6b2d0d42184b2eabfa56df3316c631
wegar2 <awegrzyn17@gmail.com>
2026-08-16 21:08:33 +0200

    implemented sll_end, reverse_sll, sll_len

--- src/dsa/linked_list/node.py
+++ src/dsa/linked_list/node.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
+from typing import Optional
 
 __all__ = ["Node"]
 
 
 class Node:
 
-    def __init__(self, val: int, next: Node):
+    def __init__(self, val: int, next: Optional[Node] = None):
         self.val: int = val
-        self.next: Node = next
+        self.next: Optional[Node] = next
--- src/dsa/linked_list/utils.py
+++ src/dsa/linked_list/utils.py
@@ -1,34 +1,64 @@
+"""
+In the remarks on complexity: n stands for length of the singly linked list
+"""
+
 from src.dsa.linked_list.node import Node
 
 __all__ = [
-    "sll_end",
-    "reverse_ll",
-    "split_at_mid",
     "attach_at_end",
-    "interweave_lls"
+    "interweave_lls",
+    "reverse_sll",
+    "sll_end",
+    "sll_len",
+    "split_at_mid"
 ]
 
 
 def sll_end(node: Node) -> Node:
-    while node.next:
+    """ Time complexity: O(n) """
+    while node.next: # noqa
         node = node.next
-    return node
+    return node # noqa
 
 
-def reverse_ll(head: Node) -> Node:
-    prev = head
-    nxt = prev.next
+def reverse_sll(head: Node) -> Node:
+    """ Time complexity: O(n) """
+    prev: Node = head
+    nxt: Node | None = prev.next
+    prev.next = None
     while nxt:
-        pass
-    return nxt
+        nxt.next = prev
+        prev, nxt = nxt, nxt.next
+    return nxt # noqa
+
+
+def sll_len(head: Node) -> int:
+    len_: int = 1
+    while head.next:
+        head = head.next
+        len_ += 1
+    return len_
 
 
 def split_at_mid(head: Node) -> tuple[Node, Node]:
-    pass
+    """
+    Time complexity: O(n).
+
+    """
+    slow = head
+    fast = head.next
+    while fast:
+        slow = slow.next
+        if fast.next and fast.next.next:
+            fast = fast.next.next
+        else:
+            pass
+    slow.next = None
+    return slow, fast
 
 
-def attach_at_end(head1: Node, head2: Node) -> Node:
-    pass
+def attach_at_end(head1: Node, head2: Node):
+    sll_end(head1).next = head2
 
 
 def interweave_lls(head1: Node, head2: Node) -> Node:
