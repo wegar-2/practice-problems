a5ff162f86588a1b1850e821c78e2e9998941472
wegar2 <awegrzyn17@gmail.com>
2026-08-18 12:05:16 +0200

    neetcode, array, longest monotonic subarray

---
+++ src/neetcode/arrays_and_hashing/longest_strictly_increasing_or_strictly_decreasing_subarray.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        inc_len = 1
+        dec_len = 1
+        out = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                inc_len += 1
+                dec_len = 1
+            elif nums[i] < nums[i - 1]:
+                inc_len = 1
+                dec_len += 1
+            else:
+                inc_len = 1
+                dec_len = 1
+
+            out = max(out, inc_len, dec_len)
+
+        return out
