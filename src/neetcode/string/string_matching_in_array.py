bda4f344e2f058105862b9c4619ef1b5590ebb90
wegar2 <awegrzyn17@gmail.com>
2026-08-18 16:43:22 +0200

    neetcode, string, string matching in array (brute force)

---
+++ src/neetcode/string/string_matching_in_array.py
@@ -0,0 +1,28 @@
+from itertools import product
+
+
+class Solution:
+    """ Brute force solution """
+
+    def stringMatching(self, words: List[str]) -> List[str]:
+        out = set()
+
+        def match_strs(subs: str, s: str) -> bool:
+            if len(subs) > len(s):
+                return False
+            for i in range(len(s) - len(subs) + 1):
+                for j in range(len(subs)):
+                    if s[i + j] != subs[j]:
+                        break
+                    if j == len(subs) - 1:
+                        return True
+            return False
+
+        for i, j in product(range(len(words)), range(len(words))):
+            if i == j:
+                continue
+            else:
+                if match_strs(words[i], words[j]):
+                    out.add(words[i])
+
+        return list(out)
