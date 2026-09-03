80d607137836b6158c22033f8941fe560aae9f6e
wegar2 <awegrzyn17@gmail.com>
2026-09-03 22:08:51 +0200

    added slightly improved version of solution2 of the majority element problem

---
+++ src/neetcode/arrays_and_hashing/majority_element_solution3.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    """
+    Brute-force suboptimal solution.
+    Worst case space complexity: O(n)
+    Slight variation of solution 3 in which collections.Counter was not used.
+    """
+    def majorityElement(self, nums: List[int]) -> int:
+        counts: Counter = Counter(nums)
+        max_count: int = max(counts.values())
+        return [
+            k for k, v in counts.items() if v == max_count
+        ][0]
