094bb2d1050b18765cf179e36d3332784b843b2e
wegar2 <awegrzyn17@gmail.com>
2026-09-12 15:30:01 +0200

    unit tests for parser of RPN

---
+++ tests/dsa/parsing/test_pn.py
@@ -0,0 +1,14 @@
+from src.dsa.parsing.pn import parse_rpn
+
+
+def test_parse_rpn1():
+    assert parse_rpn(["10", "3", "+"]) == 13
+    assert parse_rpn(["10", "3", "-"]) == 7
+    assert parse_rpn(["10", "3", "//"]) == 3
+    assert parse_rpn(["10", "3", "*"]) == 30
+
+
+def test_parse_rpn2():
+    tokens = ["10", "3", "+", "2", "3", "*", "-"]
+    assert parse_rpn(tokens) == 7
+    assert parse_rpn(tokens + ["10", "5", "//", "*"]) == 14
