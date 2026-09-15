52c5915c19dccaab0d25bfd3998bc7aa5625c915
wegar2 <awegrzyn17@gmail.com>
2026-09-15 17:40:43 +0200

    started notes on prefix function

---
+++ src/dsa/string/prefix_function.py
@@ -0,0 +1,12 @@
+
+
+def prefix_function(s: str) -> list[int] | None:
+
+    if not s:
+        return None
+
+    out: list[int] = [0]
+    for i in range(1, len(s)):
+        pass
+
+    return out
