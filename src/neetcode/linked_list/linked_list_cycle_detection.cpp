389172c515864a453f5f5940c5052be3855a90d3
wegar2 <awegrzyn17@gmail.com>
2026-09-15 19:27:19 +0200

    C++ implementation of solution to cycle detection in a linked list

---
+++ src/neetcode/linked_list/linked_list_cycle_detection.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next != nullptr and fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
