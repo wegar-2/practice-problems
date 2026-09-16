a25b445d57c335e86e751249f879d663f631d37a
wegar2 <awegrzyn17@gmail.com>
2026-09-16 17:56:12 +0200

    neetcode, find the duplicate number

---
+++ src/neetcode/two_pointers/find_the_duplicate_number.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        if len(nums) == 2:
+            return nums[0]
+
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow, fast = nums[slow], nums[nums[fast]]
+        else:
+            slow = 0
+            while nums[slow] != nums[fast]:
+                slow, fast = nums[slow], nums[fast]
+            else:
+                return nums[slow]
